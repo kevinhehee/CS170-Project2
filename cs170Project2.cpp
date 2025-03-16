eb8e276a7a06604acd8cd5557279c7c62a64249e
Kevin He <qweasd602og@gmail.com>
2025-03-16 15:05:25 -0700

    forward selection and backward elimination

--- cs170Project2.cpp
+++ cs170Project2.cpp
@@ -7,178 +7,237 @@
 
 using namespace std;
 
+void printSet(const set<int> &s) {
+    cout << "{";
+    int counter = 0;
+    for (int val: s) {
+        cout << val;;
 
+        if (++counter != s.size()) {
+            cout << ", ";
+        }
+    }
+    cout << "}";
+}
 
+vector<double> getFeatures(const vector<vector<double>> &data, const int featureObjInd, const set<int> &featureSet) {
+    vector<double> featureValues;
+    for (int feature : featureSet) {
+        featureValues.push_back(data.at(featureObjInd).at(feature));
+    }
+    return featureValues;
+}
 
-// double leaveOneOutCrossValidation(vector<vector<double>> &data, set<double> &currFeatureSet, int k) {
+double getEuclideanDistance(const vector<double> &currFeatures, const vector<double> &neighborFeatures) {
+    double sum = 0.0;
+    for (int l = 0; l < currFeatures.size(); l++ ) {
+        double difference = currFeatures.at(l) - neighborFeatures.at(l);
+        sum += difference * difference;
+    }
+    return sqrt(sum);
+}
 
-    
-
-//     return 0;
-// }
-
-double leaveOneOutCrossValidation(const vector<vector<double>> &data, set<double> featureSet, int k) {
+double leaveOneOutCrossValidation(const vector<vector<double>> &data, const set<int> &featureSet) {
     int numberClassifiedCorrectly = 0;
 
-    featureSet.insert(k);
-    cout << "TESTING FEATURES: ";
-    for (auto &entry: featureSet) {
-        cout << entry << " ";
-    }
-    cout << "\n";
-    // cout << "\n\n\n";
-
-    for (int i = 0; i < data.size(); i++) {
-
-        double currClass = data.at(i).at(0);
-
-        vector<double> currFeatures;
-        for (int feature : featureSet) {
-            currFeatures.push_back(data[i][feature]);
-        }
-
-        // cout << "Looping over " << i << endl;
-        // cout << "Object " << i << " is class " << currClass << endl;
+    for (int currInd = 0; currInd < data.size(); currInd++) {
+        double currClass = data.at(currInd).at(0);
+        vector<double> currFeatures = getFeatures(data, currInd, featureSet);
 
         double nearestNeighborDistance = INT_MAX;
-        double nearestNeighborLocation = INT_MAX;
-        double nearestNeighborClass = INT_MAX;
+        double nearestNeighborLocation = -1;
+        double nearestNeighborClass = -1;
 
-        for (int j = 0; j < data.size(); j++) {
-
-            if (i == j) {
+        for (int neighborInd = 0; neighborInd < data.size(); neighborInd++) {
+            if (currInd == neighborInd) {
                 continue;
             }
 
-            // cout << "Ask if " << i << " is nearest neighbor with " << j << endl;
-            
-            vector<double> neighborFeatures;
-            for (int feature : featureSet) {
-                neighborFeatures.push_back(data[j][feature]);
-            }
-        
-            double sum = 0;
-            for (int l = 0; l < currFeatures.size(); l++ ) {
-                sum += pow(currFeatures.at(l) - neighborFeatures.at(l), 2);
-            }
-            double distance = sqrt(sum);
+            vector<double> neighborFeatures = getFeatures(data, neighborInd, featureSet);
 
-            if (nearestNeighborDistance >= distance) {
+            double distance = getEuclideanDistance(currFeatures, neighborFeatures);
+            if (distance < nearestNeighborDistance - 1e-6) {
                 nearestNeighborDistance = distance;
-                nearestNeighborLocation = j;
-                nearestNeighborClass = data.at(j).at(0);
+                nearestNeighborLocation = neighborInd;
+                nearestNeighborClass = data.at(neighborInd).at(0);
             }
         }
-        // cout << "Object " << i << " is class " << objectClass << endl;
-        // cout << "Its nearest neighbor is " << nearestNeighborLocation << " which is class " << nearestNeighborClass << endl;
-        // cout << endl;
-        if (currClass == nearestNeighborClass) {
+
+        if (fabs(currClass - nearestNeighborClass) < 1e-6) {
             numberClassifiedCorrectly++;
         }
     }
+    cout << "\tUsing features ";
+    printSet(featureSet);
 
-
-    cout << numberClassifiedCorrectly << " / " << data.size() << endl;
-
-    double accuracy = ((double) numberClassifiedCorrectly) / data.size();
+    double accuracy = ((double) numberClassifiedCorrectly) / data.size() * 100;
+    cout << " - " << accuracy << "% accuracy" << endl;
     return accuracy;
 }
 
 
 void forwardSelectionFeatureSearch(const vector<vector<double>> &data) {
-
-    set<double> currFeatureSet;
-
-    
-    set<double> bestFeatureSet;
+    set<int> currFeatureSet;
+    set<int> bestFeatureSet;
     double maxTotalAccuracy = 0;
 
-    for (int i = 1; i < data.at(0).size(); i++) {
-        cout << "On level " << i << " of the search" << endl;
-        cout << "Current feature set: " << endl;
-        for (auto& entry: currFeatureSet) {
-            cout << entry << " ";
-        }
+    for (int searchLevel = 1; searchLevel < data.at(0).size(); searchLevel++) {
+        cout << "Level " << searchLevel << " starting feature set: ";
+        printSet(currFeatureSet);
         cout << endl;
 
         int featureToAdd = -1;
         double maxLevelAccuracy = 0;
         
-        for (int k = 1; k < data.at(0).size(); k++) {
-            if (currFeatureSet.count(k)) {
+        for (int feature = 1; feature < data.at(0).size(); feature++) {
+            if (currFeatureSet.count(feature)) {
                 continue;
             }
-            cout << "--Considering adding feature " << k << endl;
+            currFeatureSet.insert(feature);
+            double accuracy = leaveOneOutCrossValidation(data, currFeatureSet);
+            currFeatureSet.erase(feature);
 
-            double accuracy = leaveOneOutCrossValidation(data, currFeatureSet, k);
-            cout << "Accuracy is " << accuracy << endl << endl;
             if (accuracy > maxLevelAccuracy) {
                 maxLevelAccuracy = accuracy;
-                featureToAdd = k;
+                featureToAdd = feature;
             }
-
         }
-        cout << "On level " << i + 1 << ", added feature " << featureToAdd << " to the current set\n\n";
-        
         currFeatureSet.insert(featureToAdd);
+
+        cout << "\tFeature set ";
+        printSet(currFeatureSet);
+        cout << " was best with " << maxLevelAccuracy << "% accuracy" << endl << endl;
+
         if (maxLevelAccuracy > maxTotalAccuracy) {
             maxTotalAccuracy = maxLevelAccuracy;
-
             bestFeatureSet = currFeatureSet;
-            cout << "GOAT FEATURE SET!: ";
-            for (auto& entry: bestFeatureSet) {
-                cout << entry << " ";
-            }
-            cout << endl << endl;
         }
-        
     }
 
-    cout << "Best feature set: " << endl;
-    for (auto& entry: bestFeatureSet) {
-        cout << entry << " ";
-    }
+    cout << "Search finished. The best feature subset is ";
+    printSet(bestFeatureSet);
     cout << endl;
-
-
 }
 
-int main(int argc, char* argv[]) {
-    cout << "Opening file: " << argv[1] << endl;
-    string inputFile = argv[1];
+void backwardEliminationFeatureSearch(const vector<vector<double>> &data) {
+    set<int> bestFeatureSet;
+    set<int> currFeatureSet;
+    for (int feature = 1; feature < data.at(0).size(); feature++) {
+        currFeatureSet.insert(feature);
+    }
+
+    double maxTotalAccuracy = 0;
+
+    for (int searchLevel = 1; searchLevel < data.at(0).size(); searchLevel++) {
+        cout << "On level " << searchLevel << " of the search" << endl;
+        cout << "Current feature set: " << endl;
+        printSet(currFeatureSet);
+        cout << endl;
+
+        int featureToRemove = -1;
+        double maxLevelAccuracy = 0;
+        
+        for (int feature = 1; feature < data.at(0).size(); feature++) {
+            if (!currFeatureSet.count(feature)) {
+                continue;
+            }
+
+            currFeatureSet.erase(feature);
+            double accuracy = leaveOneOutCrossValidation(data, currFeatureSet);
+            currFeatureSet.insert(feature);
+
+            if (accuracy > maxLevelAccuracy) {
+                maxLevelAccuracy = accuracy;
+                featureToRemove = feature;
+            }
+
+        }
+        currFeatureSet.erase(featureToRemove);
+
+        cout << "\tFeature set ";
+        printSet(currFeatureSet);
+        cout << " was best with " << maxLevelAccuracy << "% accuracy" << endl << endl;
+
+        if (maxLevelAccuracy > maxTotalAccuracy) {
+            maxTotalAccuracy = maxLevelAccuracy;
+            bestFeatureSet = currFeatureSet;
+        }
+    }
+
+    cout << "Search finished. The best feature subset is ";
+    printSet(bestFeatureSet);
+    cout << endl;
+}
+
+vector<vector<double>> readData(const string &inputFile) {
     ifstream inFS(inputFile);
 
     if (!inFS.is_open()) {
         cout << "File not opened!" << endl;
-        return 1;
+        exit(1);
     }
 
     string line;
-
-
     int lineNum = 1;
-
     vector<vector<double>> data;
 
     while (getline(inFS, line)) {
-
         istringstream iss(line);
-
         vector<double> values;
         
         double val;
         while (iss >> val) {
             values.push_back(val);
         }
-
         data.push_back(values);
+    }
+    return data;
+}
 
+int main(int argc, char* argv[]) {
+    string inputFile = "";
+    int choice = -1;
+
+    cout << "Welcome to Kevin He's Feature Selection Program" << endl;
+    if (argc == 3) {
+        cout << "Opening file: " << argv[1] << endl << endl;
+        inputFile = argv[1];
+        choice = stoi(argv[2]);
+    } else {
+        cout << "Type in the name of the file to test: ";
+        cin >> inputFile;
+        cout << endl;
+
+        cout << "Type the number of the algorithm you want to run: " << endl;
+        cout << "\t1) Forward Selection" << endl;
+        cout << "\t2) Backward Elimination" << endl << endl;
+        cin >> choice;
     }
 
-    forwardSelectionFeatureSearch(data);
+    vector<vector<double>> data = readData(inputFile);
 
+    if (choice == 1) {
+        cout << "Starting Forward Selection\n\n";
+        auto startTime = chrono::high_resolution_clock::now();
+        forwardSelectionFeatureSearch(data);
+        auto endTime = chrono::high_resolution_clock::now();
 
+        chrono::duration<double> elapsedSeconds = endTime - startTime;
+        double minutes = elapsedSeconds.count() / 60.0;
 
+        cout << std::fixed << std::setprecision(2) << "Forward Selection took " << elapsedSeconds.count() << " seconds or " << minutes << " minutes" << endl;
+    }
+    else if (choice == 2) {
+        cout << "Starting Backward Elimination\n\n";
+        auto startTime = chrono::high_resolution_clock::now();
+        backwardEliminationFeatureSearch(data);
+        auto endTime = chrono::high_resolution_clock::now();
+        
+        chrono::duration<double> elapsedSeconds = endTime - startTime;
+        double minutes = elapsedSeconds.count() / 60.0;
+
+        cout << std::fixed << std::setprecision(2) << "Backward Elimination took " << elapsedSeconds.count() << " seconds or " << minutes << " minutes" << endl;
+    }
 
 
     return 0;
